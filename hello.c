3cbebbaf76a6dc49700c45755b0e10767f4a42e5
Dominic Paul <dpaulcollege@gmail.com>
2018-01-23 15:29:05 -0600

    Added Hello Dominic File

---
+++ hello.c
@@ -0,0 +1,13 @@
+/**
+  Author: Dominic Paul
+  Date: xx/xx/20xx
+  Hello World in C 
+*/ 
+
+#include <stdio.h> 
+ 
+int main(int argc, char **argv) 
+{
+  printf("Hello Dominic!\n");
+  return 0;
+}
