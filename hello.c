ab0ca2f8bad049d181a4c0250c86ed5604caa1dd
DatNguyen <datnguyen5653@huskers.unl.edu>
2018-01-23 15:38:30 -0600

    New commit- I added my name

--- hello.c
+++ hello.c
@@ -1,13 +1,13 @@
 /**
   Author: Dominic Paul
   Date: xx/xx/20xx
-  Hello World in C 
-*/ 
+  Hello World in C
+*/
 
-#include <stdio.h> 
- 
-int main(int argc, char **argv) 
+#include <stdio.h>
+
+int main(int argc, char **argv)
 {
-  printf("Hello Dominic!\n");
+  printf("Hello Dominic! I am Dat \n");
   return 0;
 }
